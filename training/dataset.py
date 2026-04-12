8a9a2e212887568f3b6a40f93ff907380c6f72c8
David Butsch <dcbutsch@gmail.com>
2026-04-12 01:31:19 -0500

    add cross-sentence training

--- training/dataset.py
+++ training/dataset.py
@@ -8,40 +8,39 @@ class TextDataset(Dataset):
     def __init__(self, tokenizer: Tokenizer):
         super().__init__()
 
-        self.tokenized_lines = []
+        self.tokenized_dataset = []
 
         # Tokenize text file lines
         with open(config["ds_path"], "r") as file:
-            lines = file.readlines()
 
-            for line in lines:
+            for line in file:
+
+                stripped_line = line.strip()
+
                 # Tokenize
-                normalized_line = tokenizer.normalize(line)  # Normalize line
+                normalized_line = tokenizer.normalize(stripped_line)  # Normalize line
                 tokens = tokenizer.tokenize(normalized_line)  # Tokenize normalized line
                 tokenizer.build_vocab(tokens)  # Add to vocabulary with line tokens
                 token_ids = tokenizer.encode(tokens)  # Get token ids from tokens
 
                 # Keep tokenized_line
-                self.tokenized_lines.append(token_ids)
-
-            self.lines = lines
+                self.tokenized_dataset.extend(token_ids)
 
     def __len__(self):
-        return len(self.lines)
+        return len(self.tokenized_dataset) // config["max_seq_len"]
 
     # Returns (input_ids, target_ids)
     def __getitem__(self, index) -> tuple[torch.Tensor, torch.Tensor]:
 
-        max_seq_len = config["max_seq_len"]
+        # Read in token ids in range [index * max_seq_len, (index + 1) * max_seq_len + 1]
 
         # Read in token ids
-        token_ids: list[int] = self.tokenized_lines[index]
+        token_ids: list[int] = self.tokenized_dataset[
+            index * config["max_seq_len"] : (index + 1) * config["max_seq_len"] + 1
+        ]
 
-        # Truncate to max_seq_len + 1 (see example for why)
-        token_ids = token_ids[: max_seq_len + 1]
-
-        # Add {pad_len} padding tokens to reach max_seq_len
-        pad_len = max_seq_len + 1 - len(token_ids)
+        # Add {pad_len} padding tokens to reach max_seq_len (if necessary)
+        pad_len = config["max_seq_len"] + 1 - len(token_ids)
         token_ids = token_ids + [config["pad_i"]] * pad_len
 
         # Build target, input tensors
